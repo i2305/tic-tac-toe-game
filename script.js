bdf8bbc6c3a8586d06486d303de1070fe14545cd
i2305 <iva.vukovic695@gmail.com>
2023-10-25 11:54:52 +0200

    add createBoard and start functions

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-console.log("hello world");
+const GameBoard = (() =>{
+    const gameboard=document.querySelector("#gameboard");
+    const startCells=["", "", "", "", "", "", "", "", ""]
+
+    const createBoard = ()=>{
+        startCells.forEach((cell, index) =>{
+            const cellElement=document.createElement("div");
+            cellElement.classList.add("cell");
+            gameboard.appendChild(cellElement)
+        });
+    }
+
+    return{
+        createBoard,
+    }
+
+})();
+
+const Game =(()=>{
+    let currentPlayer;
+    let gameOver;
+
+    const start=()=>{
+        const X=document.querySelector("#X-button");
+        X.addEventListener("click", function(){
+        const info=document.querySelector(".info");
+        info.textContent="X plays first"
+        });
+
+        const O=document.querySelector("#O-button");
+        O.addEventListener("click", function(){
+        const info=document.querySelector(".info");
+        info.textContent="O plays first"
+        });
+        
+    };
+    currentPlayer=0;
+    gameOver=false;
+
+    return{
+        start,
+    }
+
+})();
+
+const startGame=document.querySelector("#start-button");
+startGame.addEventListener("click", function(){
+    GameBoard.createBoard();
+    Game.start();
+},{once:true});
